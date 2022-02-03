8fd7e9ba2a6dd41a8a6be41f3e6dcec2057c2fac
Ezra <ezradecleen@hotmail.com>
2022-02-03 11:29:19 +0100

    update

--- __Main/rooster.py
+++ __Main/rooster.py
@@ -6,23 +6,24 @@ import malus_calc
 import numpy as np
 
 
+"""
+A random base solution is created by single_loop
 
-# a random base solution is created by single_loop
+the rest of the functions are used to apply a certain algoritm for a certain amount of time.
 
-# the rest of the functions are used to apply a certain algoritm for a certain amount of time.
+The arguements of these functions are: seconds,timeout_amount, change_amount, solution
+seconds: The time you want the program to run in seconds
+timeout_amount: after n consecutive attempted changes with no decrease in maluspoints the loop will stop 
+(even before the times, so set this to a high number if y)
+"""
 
-# The arguements of these functions are: seconds,timeout_amount, change_amount, solution
-# seconds: The time you want the program to run in seconds
-# timeout_amount: after n consecutive attempted changes with no decrease in maluspoints the loop will stop 
-# (even before the times, so set this to a high number if y)
-
-""""
+"""
 solution = single_loop()
 switch.activity_switch_emptyslot(seconds,timeout_amount,-1, solution)
 switch.activity_switch(seconds,timeout_amount,-1, solution)
 switch.student_switch_emptyslot(seconds,timeout_amount,-1, solution)
 switch.student_switch(seconds,timeout_amount,-1, solution)
-"""""
+"""
 
 
 week_data,student_data,activity_list,total_changes,total_attempts, malus_list_changes, malus_list_tries = looper.loop_tuner(30,30,500,500, 30)
